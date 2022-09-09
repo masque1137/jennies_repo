e8ff996c7e488dd9c049315937214b642667c318
Chris Woolley <cwoolley1@gmail.com>
2022-09-09 00:19:15 -0400

    iterates through files and builds list of lists

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
-import pypdf
+import PyPDF2
 import os
 
-# creating a pdf file object 
-pdfFileObj = open('\data\minutes.pdf', 'rb') 
-    
-# creating a pdf reader object 
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
-    
-# printing number of pages in pdf file 
-print(pdfReader.numPages) 
-    
-# creating a page object 
-pageObj = pdfReader.getPage(0) 
-    
-# extracting text from page 
-print(pageObj.extractText()) 
-    
-# closing the pdf file object 
-pdfFileObj.close() 
+thisdir = os.getcwd()
+
+data_dir = thisdir + '\data'
+ext = ('.pdf')
+allfiles =[]
+for files in os.listdir(data_dir):
+    if files.endswith(ext):
+
+
+        # creating a pdf file object 
+        pdfFileObj = open(thisdir + '\\' + files, 'rb') 
+            
+        # creating a pdf reader object 
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
+            
+        # printing number of pages in pdf file 
+        print(pdfReader.numPages) 
+            
+
+        # pageObj = pdfReader.getPage(0) 
+        allpages=[]
+        for thispageno in range(0,pdfReader.numPages-1):
+            thispage = pdfReader.getPage(thispageno)
+            thispagetext = thispage.extract_text()
+            print(thispagetext) 
+            allpages.append(thispagetext)
+        
+        allfiles.append(allpages)
+        # closing the pdf file object 
+        pdfFileObj.close()  
+    else:
+        continue
+        
